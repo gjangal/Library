6fdd9756ecda86c57d6585d10aadea5bc8a3d617
gjangal <ganesh.gj@gmail.com>
2017-08-05 22:57:12 -0400

    Adding Library Data

---
+++ LibraryData/Models/Checkout.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Checkout
+    {
+        public int Id { get; set; }
+    }
+}
