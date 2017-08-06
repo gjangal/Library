03ae121191a26c24a582740d961a2abc79242984
gjangal <ganesh.gj@gmail.com>
2017-08-05 23:06:27 -0400

    Added Checkout Model

--- LibraryData/Models/Checkout.cs
+++ LibraryData/Models/Checkout.cs
@@ -7,5 +7,9 @@ namespace LibraryData.Models
     public class Checkout
     {
         public int Id { get; set; }
+        public LibraryAsset LibraryAsset { get; set; }
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime Since { get; set; }
+        public DateTime Until { get; set; }
     }
 }
