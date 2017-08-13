f20949c26ca74a37b023ee26f7fff8a3c90b361b
gjangal <ganesh.gj@gmail.com>
2017-08-12 20:31:53 -0400

    Checkout Service WIP. Neet to implement PlaceHold, CheckIn and CheckOut item

--- LibraryData/ICheckout.cs
+++ LibraryData/ICheckout.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryData.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,23 @@ namespace LibraryData
 {
     public interface ICheckout
     {
+        IEnumerable<Checkout> GetAll();
+        Checkout GetById(int id);
+
+        void Add(Checkout newCheckout);
+        void CheckInItem(int assetId, int libraryCardId);
+        void CheckOutItem(int assetId, int libraryCardId);
+
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int assetId);
+
+        //Hold Actions
+        void PlaceHold(int assetId, int libraryCardId);
+        string GetCurrentHoldPatronName(int id);
+        DateTime GetCurrentHoldPlaced(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+        Checkout GetLatestCheckout(int id);
+
+        void MarkLost(int assetId);
+        void MarkFound(int assetId);
     }
 }
---
+++ LibraryServices/CheckoutService.cs
@@ -0,0 +1,117 @@
+﻿using LibraryData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryData.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryServices
+{
+    public class CheckoutService : ICheckout
+    {
+        private readonly LibraryContext _context;
+
+        public CheckoutService(LibraryContext context)
+        {
+            _context = context; 
+        }
+
+        public void Add(Checkout newCheckout)
+        {
+            _context.Add(newCheckout);
+            _context.SaveChanges();
+        }
+
+        public void CheckInItem(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOutItem(int assetId, int libraryCardId)
+        {
+            
+        }
+
+        public IEnumerable<Checkout> GetAll()
+        {
+            return _context.Checkouts;
+        }
+
+        public Checkout GetById(int id)
+        {
+            return _context.Checkouts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int assetId)
+        {
+            return _context.CheckoutHistories
+                .Include(c=>c.LibraryAsset)
+                .Include(c=>c.LibraryCard)
+                .Where(c => c.LibraryAsset.Id == assetId);
+        }
+
+        public string GetCurrentHoldPatronName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCurrentHoldPlaced(int id)
+        {
+            return GetCurrentHolds(id).FirstOrDefault().HoldPlaced;
+        }
+
+        public IEnumerable<Hold> GetCurrentHolds(int id)
+        {
+            return _context.Holds
+                .Include(h=>h.LibraryAsset)
+                .Include(h=>h.LibraryCard)
+                .Where(h => h.LibraryAsset.Id == id);
+        }
+
+        public Checkout GetLatestCheckout(int id)
+        {
+            return _context.Checkouts
+                .Where(c=>c.LibraryAsset.Id == id)
+                .OrderByDescending(c=>c.Since).FirstOrDefault();
+        }
+
+        public void MarkFound(int assetId)
+        {
+            var item = _context.LibraryAssets
+                .FirstOrDefault(a => a.Id == assetId);
+
+            _context.Update(item);
+            item.Status = _context.Statuses
+                .FirstOrDefault(s => s.Name == "Lost");
+
+            _context.SaveChanges();
+        }
+
+        public void MarkLost(int assetId)
+        {
+            var item = _context.LibraryAssets
+                .FirstOrDefault(a => a.Id == assetId);
+
+            _context.Update(item);
+
+            item.Status = _context.Statuses
+                .FirstOrDefault(s => s.Name == "Available");
+
+            var checkout = _context.Checkouts.FirstOrDefault(c => c.LibraryAsset.Id == assetId);
+
+            if(checkout != null)
+            {
+                _context.Remove(checkout);
+            }
+
+            _context.SaveChanges();
+
+        }
+
+        public void PlaceHold(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
